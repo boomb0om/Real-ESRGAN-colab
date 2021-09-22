de627143f779402a33e01de70f8d444d296b102a
boomb0om <igorekandco1@gmail.com>
2021-09-22 20:52:56 +0300

    Made this repo as module

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .realesrgan import RealESRGAN
+
+__all__ = ["RealESRGAN"]
--- realesrgan.py
+++ realesrgan.py
@@ -4,8 +4,8 @@ from PIL import Image
 import numpy as np
 import cv2
 
-from rrdbnet_arch import RRDBNet
-from utils_sr import *
+from .rrdbnet_arch import RRDBNet
+from .utils_sr import *
 
 
 class RealESRGAN:
